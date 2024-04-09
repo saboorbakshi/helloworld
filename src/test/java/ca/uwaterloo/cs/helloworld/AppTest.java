bfff8e3de85e6db46dbd5d5ec3c98ea810e06d05
Saboor Bakshi <saboor@Saboors-MacBook.local>
2024-04-09 18:26:38 -0400

    break

--- src/test/java/ca/uwaterloo/cs/helloworld/AppTest.java
+++ src/test/java/ca/uwaterloo/cs/helloworld/AppTest.java
@@ -15,6 +15,8 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( false );
+        assertTrue( false );
+        assertTrue( false );
     }
 }
